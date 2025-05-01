81eab686ed2b0252f295b49ac339ce00faa87bd8
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2025-05-01 12:08:20 -0500

    Completed Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let chars = string.split("");
+
+    let reversed = [];
+
+    let i = string.length - 1;
+
+    while(i > -1){
+
+        //console.log(i + " " + chars[i]);
+        reversed.push(chars[i]);
+        i--;
+    }
+
+    reversedstring = reversed.join("");
+
+    return reversedstring;
 
 };
 
