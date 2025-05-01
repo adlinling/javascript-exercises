c777952c973d6b99728ddae3d5edf306759b8045
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2025-05-01 07:13:45 -0500

    Completed Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let str = "";
+
+    if(num < 0){
+
+        str = "ERROR";
+
+    }else{
+
+        for(let i=1; i <=num; i++){
+            //console.log(i);
+            str += string;
+        }
+
+    }
+
+   
+
+    //console.log(str);
+    return str;
 };
 
 // Do not edit below this line
