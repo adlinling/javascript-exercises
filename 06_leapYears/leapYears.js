7ebf6edf82cf049ee75f3b247bb175aa13c759a2
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2025-05-03 10:12:59 -0500

    Completed Exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+const leapYears = function(yearTocheck) {
+    
+    console.log("Checking year: " + yearTocheck);
+
+    if(yearTocheck % 400 == 0) return true;
+
+
+    if(yearTocheck % 4 == 0){
+
+        if(yearTocheck % 100 !== 0){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }else{
+        return false;
+    }
 
 };
 
