08eaaa837bee2f97be2121edeccf10623fce34a1
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2025-05-02 20:47:05 -0500

    Completed Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...removethese) {
+
+    let newArray = [];
+
+    console.log(removethese);
+
+    for (element of inputArray){
+        
+        console.log(element);
+        
+        //If element is inside the removethese array, the value of removethese.indexOf(element) will be > -1, i.e. returns the index location in the removethese array
+        if(removethese.indexOf(element) !== -1){
+
+            console.log(element + " will be removed");
+
+        }else{
+            console.log(element + " can stay");
+            newArray.push(element);
+        }
+        
+    }
+
+    //console.log(newArray);
+    return newArray;
+    
 };
 
 // Do not edit below this line
