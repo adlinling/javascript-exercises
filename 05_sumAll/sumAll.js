52e71223186c77d7425c42031cdd95eb9dade625
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2025-05-02 21:19:55 -0500

    Completed Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    let i = start;
+    let sum = 0;
+
+    if(start < 0 || end < 0) return "ERROR";
+
+    if(Math.floor(start) !== start || Math.floor(end) !== end)  return "ERROR";
+
+    if(typeof start !== "number" || typeof end !== "number") return "ERROR";
+
+    if(start < end){
+        
+        while(i <= end){
+
+            console.log(i);
+            sum += i;
+            i++;
+        }
+
+    }else{
+
+        while(i >= end){
+            console.log(i);
+            sum += i;
+            i--;
+        }
+
+    }
+
+    //console.log("Total: " + sum);
+    return sum;
+    
 };
 
 // Do not edit below this line
