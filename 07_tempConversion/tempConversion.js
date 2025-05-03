a8fe58bfde279dd3841f7f4261289493278f1ac8
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2025-05-03 11:03:06 -0500

    Completed Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+
+  let C = (Number(F) - 32)*5/9;
+
+  if(Number.isInteger(C)){
+      return Number(C);
+  }else{
+      return Number(C.toFixed(1));
+  }
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+
+  let F = Number(C)*(9/5) + 32;
+
+  if(Number.isInteger(F)){
+      return Number(F);
+  }else{
+      return Number(F.toFixed(1));
+  }
+
 };
 
 // Do not edit below this line
