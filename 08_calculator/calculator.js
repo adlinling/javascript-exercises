d85b8efcc2506136a0679e7a4536e4053f746ad3
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2025-05-03 22:08:05 -0500

    Completed Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,61 @@
-const add = function() {
+const add = function(a, b) {
+
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+
+  return Number(a) - Number(b);
 	
 };
 
-const sum = function() {
+const sum = function(numbers) {
+
+  let sum = 0;
+
+  for(num of numbers){
+    sum += num;
+  }
+
+  return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
 
+  let final = 1;
+
+  for(num of numbers){
+
+      final *= num;
+      console.log(final);
+  }
+
+  return final;
+  
 };
 
-const power = function() {
-	
+
+
+const power = function(number, exponent) {
+    return number ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+       
+  if(number == 0) return 1;
+
+  let factorial = number;
+
+  for (let i = number - 1; i > 0; i--){
+      console.log(i);
+      factorial *= i;
+  }
+
+  console.log("Final: " + factorial);
+  return factorial;
+  
 };
 
 // Do not edit below this line
