0780abd75ae483fb95d904c8f331d8270cb62138
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2025-05-04 01:44:48 -0500

    Completed Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,34 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    let num = Number(number);
+
+    if(num == 0) return 0;
+
+    if(num < 0) return "OOPS";
+
+    
+    let sum;
+    let seq = [];
+    seq[0] = 1;
+    seq[1] = 1;
+
+    for(let i = 1; i < num - 1; i++){
+
+        previndex = i - 1;
+
+        nextindex = i + 1;
+
+        sum = seq[i] + seq[previndex];
+        
+        seq[nextindex] = sum;
+
+        //console.log(seq[nextindex]);
+    
+    }
+
+    console.log(seq);
+
+    return seq.pop();
 };
 
 // Do not edit below this line
