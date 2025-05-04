93f100852853d32bbaaeaa4a6ad1849a583984bc
Adlin Ling <3029324+adlinling@users.noreply.github.com>
2025-05-04 15:09:41 -0500

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,42 @@
-const findTheOldest = function() {
+const findTheOldest = function(peeps) {
+
+    const date = new Date();
+
+    const thisyear = date.getFullYear();
+    console.log(thisyear);
+
+    let maxage = 0;
+    let olderperson = "unknown";
+
+    //reduce() is a looping function
+    let oldest = peeps.reduce((older, person) => {
+
+      console.log(person.name);
+      
+      console.log("Born: " + person.yearOfBirth);
+
+      if(person.yearOfDeath){
+          console.log("Died: " + person.yearOfDeath);
+          age = person.yearOfDeath - person.yearOfBirth;
+      }else{
+          console.log("Still living or unknown death year");
+          age = thisyear - person.yearOfBirth;
+      }
+
+      console.log("Age of "+ person.name + ": " + age);
+
+      if(age > maxage){
+        console.log(person.name + " is older than " + maxage);
+        older = person;
+        maxage = age;
+      }
+
+      return older;
+
+    }, olderperson);
+
+
+    return oldest;
 
 };
 
